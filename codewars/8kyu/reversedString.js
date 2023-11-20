e18625c99c3e4ea1d69c59bf79a9dbc96ead9440
Theglxry <102861719+Theglxry@users.noreply.github.com>
2023-11-20 04:36:57 +0100

    reversed a string

--- codewars/8kyu/reversedString.js
+++ codewars/8kyu/reversedString.js
@@ -1,5 +1,16 @@
 
 
 
-// Complete the solution so that it reverses the string passed into it.
 
+// Complete the solution so that it reverses the string passed into it.
+/*
+ //1-> first convert string into an array by using the split method which converts the word into array of split words.['h','e','y']
+ //2->-reverse() is an array method , hence the conversion */
+
+
+
+
+ 
+function solution(str) {
+  return str.split("").reverse("").join("");
+}
