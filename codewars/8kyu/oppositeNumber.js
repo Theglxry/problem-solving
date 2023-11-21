dd7c2c0bbe816f06564a61e728e5c5a7b41fc10e
Theglxry <102861719+Theglxry@users.noreply.github.com>
2023-11-21 06:09:06 +0100

    makeNegative

--- codewars/8kyu/oppositeNumber.js
+++ codewars/8kyu/oppositeNumber.js
@@ -3,3 +3,13 @@
 function opposite(number) {
   return number * -1;
 }
+
+
+// - RELATED EXERCISE
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+  function makeNegative(num) {
+   return num >= 0 ? num * -1 : num
+    
+  }
+
